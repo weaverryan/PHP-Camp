f28a1ee51d2b143c569f820d2fa301444760cd7e
Ryan Weaver <ryan@thatsquality.com>
2014-11-12 05:51:10 -0500

    [tuts-apply-diff] Applying existing diff file for step symfony-httpfoundation

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__.'/bootstrap.php';
 
 try {
     $dbPath = __DIR__.'/data/database.sqlite';
@@ -7,33 +8,46 @@ try {
     die('Panic! '.$e->getMessage());
 }
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// create a request object to help us
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$request = Request::createFromGlobals();
+
+$uri = $request->getPathInfo();
 
 if ($uri == '/' || $uri == '') {
 
-    echo '<h1>PHP Camp!</h1>';
-    echo '<a href="/attendees">See the attendees</a>';
-    if (isset($_GET['name'])) {
-        echo sprintf('<p>Oh, and hello %s!</p>', $_GET['name']);
+    $content = '<h1>PHP Camp!</h1>';
+    $content .= '<a href="/attendees">See the attendees</a>';
+    if ($name = $request->query->get('name')) {
+        $content .= sprintf('<p>Oh, and hello %s!</p>', $name);
     }
+
+    $response = new Response($content);
 
 } elseif ($uri == '/attendees') {
 
     $sql = 'SELECT * FROM php_camp';
-    echo '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />';
-    echo '<h1>PHP Camp Attendees</h1>';
-    echo '<table class="table" style="width: 300px;">';
+    $content = '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />';
+    $content .= '<h1>PHP Camp Attendees</h1>';
+    $content .= '<table class="table" style="width: 300px;">';
     foreach ($dbh->query($sql) as $row) {
-        echo sprintf(
+        $content .= sprintf(
             '<tr><td style="font-size: 24px;">%s</td><td><img src="%s" height="120" /></td></tr>',
             $row['attendee'],
             $row['avatar_url']
         );
     }
-    echo '</table>';
+    $content .= '</table>';
+
+    $response = new Response($content);
 
 } else {
-    header("HTTP/1.1 404 Not Found");
-    echo '<h1>404 Page not Found</h1>';
-    echo '<p>Find a boy (or girl) scout - they can fix this!</p>';
+    $content = '<h1>404 Page not Found</h1>';
+    $content .= '<p>Find a boy (or girl) scout - they can fix this!</p>';
+
+    $response = new Response($content);
+    $response->setStatusCode(404);
 }
+
+$response->send();
